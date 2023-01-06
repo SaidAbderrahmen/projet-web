a3d5da5aae711bce3755efca759916dc0a3724a9
Abderrahmen Said <said10abderrahmen@gmail.com>
2023-01-06 23:10:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
+}
+fade($('.quoteLoop > .quote').first());
+
